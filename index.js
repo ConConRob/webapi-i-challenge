070514f1803aefac7514910b8a56773dd65f7916
conconrob <connortprobertson@gmail.com>
2019-03-18 12:58:50 +0000

    done and tested read user by id

--- index.js
+++ index.js
@@ -25,8 +25,21 @@ server.get('/api/users', (req,res) => {
 
 server.get('/api/users/:id', (req,res) => {
   db.findById(req.params.id)
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(404).json({ error: "The user information could not be retrieved." }))
+    .then(data => {
+      if(!data) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }else {
+        res.status(200).json(data)
+      } 
+      
+    })
+    .catch(()=> res.status(500).json({ error: "The user information could not be retrieved." }))
+    
+    
 })
+
+// server.delete('/api/users/:id', (req,res => {
+
+// }))
 
 server.listen(4000, () => console.log('Listening on port 4000'));
