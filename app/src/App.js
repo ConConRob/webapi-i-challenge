d16feeaa1c7dd68bd782fe2f8711b4261b336f72
conconrob <connortprobertson@gmail.com>
2019-03-18 14:29:07 +0000

    post a new friend is working in react app

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,9 @@ const url = "http://127.0.0.1:4000/api/users";
 
 class App extends Component {
   state = {
-    friends: []
+    friends: [],
+    inputName: "",
+    inputBio: ""
   };
   componentDidMount() {
     axios.get(url).then(res => this.setState({ friends: res.data }));
@@ -22,14 +24,53 @@ class App extends Component {
     });
   };
 
+  handleUpdate = friend => {
+    const something = "";
+  };
+
+  handleInputNameChange = event => {
+    this.setState({ inputName: event.target.value });
+  };
+
+  handleInputBioChange = event => {
+    this.setState({ inputBio: event.target.value });
+  };
+
+  handleAddFriend = () => {
+    axios
+      .post(url, { name: this.state.inputName, bio: this.state.inputBio })
+      .then(res => {
+        this.setState(curState => ({
+          friends: [...curState.friends, res.data]
+        }))
+        this.setState({inputName:'', inputBio:''})
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <div className="friends">
+          <input
+            placeholder="Name"
+            onChange={this.handleInputNameChange}
+            value={this.state.inputName}
+          />
+          <input
+            placeholder="bio"
+            onChange={this.handleInputBioChange}
+            value={this.state.inputBio}
+          />
+          <button onClick={this.handleAddFriend}>Add Friend</button>
           {this.state.friends.map(friend => (
             <div key={friend.id} className="friend">
               <p>{friend.name}</p>
-              <button onClick={()=>this.handleDelete(friend.id)} >Delete</button>
+              <button onClick={() => this.handleDelete(friend.id)}>
+                Delete
+              </button>
+              <button onClick={friend => this.handleUpdate(friend)}>
+                Update
+              </button>
             </div>
           ))}
         </div>
