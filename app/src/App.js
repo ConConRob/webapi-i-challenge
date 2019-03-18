9e38b05e7ed2c35f214f22dba533847d9dd1076b
conconrob <connortprobertson@gmail.com>
2019-03-18 14:08:09 +0000

    delete a friend is working in react app

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-const url="http://127.0.0.1:4000/api/users"
+const url = "http://127.0.0.1:4000/api/users";
 
 class App extends Component {
-  state={
-    friends:[]
+  state = {
+    friends: []
+  };
+  componentDidMount() {
+    axios.get(url).then(res => this.setState({ friends: res.data }));
   }
-  componentDidMount(){
-    console.log("here")
-    axios.get(url)
-    .then((res)=> this.setState({friends: res.data}))
-  }
-  
+
+  handleDelete = id => {
+    axios.delete(`${url}/${id}`).then(() => {
+      this.setState(curState => {
+        const newFriends = curState.friends.filter(friend => friend.id !== id);
+        return {
+          friends: newFriends
+        };
+      });
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <div className="friends" >
-        {
-          this.state.friends.map(friend => <div className="friend"> {friend.name} </div>)
-        }
+        <div className="friends">
+          {this.state.friends.map(friend => (
+            <div key={friend.id} className="friend">
+              <p>{friend.name}</p>
+              <button onClick={()=>this.handleDelete(friend.id)} >Delete</button>
+            </div>
+          ))}
         </div>
       </div>
     );
