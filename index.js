c7e4ceb53e8e38765a47f71d87724d5ae1ce3c70
conconrob <connortprobertson@gmail.com>
2019-03-18 13:29:34 +0000

    made create react app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // implement your API here
 
 const express = require('express');
+const cors = require('cors')
 const db = require('./data/db')
 
 const server = express()
 
 server.use(express.json());
+server.use(cors());
 
 server.post('/api/users', (req, res) => {
   const {name, bio} = req.body;
